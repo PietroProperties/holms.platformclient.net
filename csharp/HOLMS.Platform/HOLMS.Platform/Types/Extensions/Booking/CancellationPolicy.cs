d64f07b00c0694fba9293e9b7948b792dd06eef9
David R. Albrecht <albrecht.dr@gmail.com>
2016-11-03 18:04:53 -0700

    Remove unused positional constructor arg

--- csharp/HOLMS.Platform/HOLMS.Platform/Types/Extensions/Booking/CancellationPolicy.cs
+++ csharp/HOLMS.Platform/HOLMS.Platform/Types/Extensions/Booking/CancellationPolicy.cs
@@ -5,7 +5,7 @@ using HOLMS.Types.Primitive;
 namespace HOLMS.Types.Booking {
     public partial class CancellationPolicy : EntityDTO<CancellationPolicyIndicator> {
         public CancellationPolicy(CancellationPolicyIndicator id, string description,
-            int noPenaltyDays, int forfeitDepositDays, CancellationFeeCategory c,
+            int noPenaltyDays, CancellationFeeCategory c,
             decimal cancellationFeeAmount, decimal cancellationFeeRate,
             string policyText) {
             EntityId = id;
