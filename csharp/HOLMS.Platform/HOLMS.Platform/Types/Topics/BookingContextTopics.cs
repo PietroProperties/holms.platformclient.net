01f0bcbf16a305be4c331df06029a51200a2563d
Derek Lax <lax.derek@gmail.com>
2016-11-13 15:11:53 -0500

    Adds ReservationCheckOutNotification and associated topic.
    [#133972513]

--- csharp/HOLMS.Platform/HOLMS.Platform/Types/Topics/BookingContextTopics.cs
+++ csharp/HOLMS.Platform/HOLMS.Platform/Types/Topics/BookingContextTopics.cs
@@ -4,6 +4,7 @@
 
         public const string ReservationCancellation = "bookings.reservations.cancellations";
         public const string ReservationCheckInTopic = "booking.reservations.checkins";
+        public const string ReservationCheckOutTopic = "booking.reservations.checkouts";
         public const string ReservationFRPAmendment = "bookings.reservations.frp_amendment";
         public const string ReservationBooked = "bookings.reservations.reservation_booked";
         public const string ReservationGuaranteeOccurred = "bookings.reservations.guarantees.any";
