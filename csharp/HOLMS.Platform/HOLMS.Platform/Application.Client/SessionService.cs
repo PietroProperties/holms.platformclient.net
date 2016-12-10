470e53ef3155fc942ff39dcaa4028a449d860022
Derek Lax <lax.derek@gmail.com>
2016-12-10 14:09:29 -0500

    SessionService / ACC can determine token's security actions.
    [#134825047]

--- csharp/HOLMS.Platform/HOLMS.Platform/Application.Client/SessionService.cs
+++ csharp/HOLMS.Platform/HOLMS.Platform/Application.Client/SessionService.cs
@@ -13,41 +13,7 @@ namespace HOLMS.Application.Client {
             if (SessionContext.User == null) {
                 return false;
             }
-
-            switch (e) {
-                case SecurityAction.AdjustFolio:
-                    return false;
-                case SecurityAction.ChangeSystemSettings:
-                    return false;
-                case SecurityAction.GetRoomTypes:
-                    return false;
-                case SecurityAction.GetSupplySnapshot:
-                    return false;
-                case SecurityAction.ManageGroups:
-                    return false;
-                case SecurityAction.ManageGuests:
-                    return false;
-                case SecurityAction.ManageHousekeeping:
-                    return false;
-                case SecurityAction.ManageIncidentalReservation:
-                    return false;
-                case SecurityAction.ManageMoney:
-                    return false;
-                case SecurityAction.ManagePricing:
-                    return false;
-                case SecurityAction.ManageReservation:
-                    return false;
-                case SecurityAction.ManageSupply:
-                    return false;
-                case SecurityAction.ManageUsers:
-                    return false;
-                case SecurityAction.PerformBackgroundJob:
-                    return false;
-                case SecurityAction.ReadUsers:
-                    return false;
-                default:
-                    return false;
-            }
+            return SessionContext.SecurityActions.Contains(e);
         }
     }
 }
