dc832218815125fe24456efad7c47ff16c1e3824
David R. Albrecht <albrecht.dr@gmail.com>
2017-02-14 18:08:19 -0800

    Add tag for migrated reservations
    Migrated reservations have different behavior in some respects, including
    how guarantees work

---
+++ csharp/HOLMS.Platform/HOLMS.Platform/Support/ReservationTags/MigratedReservationTag.cs
@@ -0,0 +1,10 @@
+﻿namespace HOLMS.Platform.Support.ReservationTags {
+    class MigratedReservationTag : ReservationTagBase {
+        protected override string[] GetDescriptorPartsAfterCategory() =>
+            new string[] { };
+
+        protected override string GetCategoryDescriptor() => MigratedBookingCategory;
+
+        public override bool IsPermanent => true;
+    }
+}
