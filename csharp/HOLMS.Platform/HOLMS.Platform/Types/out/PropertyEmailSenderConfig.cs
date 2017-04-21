61ae56edda08793678c10db6cd59dfee368df052
pcsouza <souza.patrick@gmail.com>
2017-04-21 12:09:52 -0700

    [#143813159] Adds global disable to automated e-mails

--- csharp/HOLMS.Platform/HOLMS.Platform/Types/out/PropertyEmailSenderConfig.cs
+++ csharp/HOLMS.Platform/HOLMS.Platform/Types/out/PropertyEmailSenderConfig.cs
@@ -24,16 +24,17 @@ namespace HOLMS.Types.TenancyConfig {
           string.Concat(
             "CjF0ZW5hbmN5X2NvbmZpZy9wcm9wZXJ0eV9lbWFpbF9zZW5kZXJfY29uZmln",
             "LnByb3RvEhpob2xtcy50eXBlcy50ZW5hbmN5X2NvbmZpZxoydGVuYW5jeV9j",
-            "b25maWcvaW5kaWNhdG9ycy9wcm9wZXJ0eV9pbmRpY2F0b3IucHJvdG8iqAEK",
+            "b25maWcvaW5kaWNhdG9ycy9wcm9wZXJ0eV9pbmRpY2F0b3IucHJvdG8i0gEK",
             "GVByb3BlcnR5RW1haWxTZW5kZXJDb25maWcSSgoIcHJvcGVydHkYASABKAsy",
             "OC5ob2xtcy50eXBlcy50ZW5hbmN5X2NvbmZpZy5pbmRpY2F0b3JzLlByb3Bl",
             "cnR5SW5kaWNhdG9yEiUKHXBvc3RtYXJrX2VtYWlsX3NlbmRlcl9hZGRyZXNz",
-            "GAIgASgJEhgKEHBvc3RtYXJrX2FwaV9rZXkYAyABKAlCHKoCGUhPTE1TLlR5",
-            "cGVzLlRlbmFuY3lDb25maWdiBnByb3RvMw=="));
+            "GAIgASgJEhgKEHBvc3RtYXJrX2FwaV9rZXkYAyABKAkSKAogZGlzYWJsZV9h",
+            "dXRvbWF0ZWRfY29ycmVzcG9uZGVuY2UYBCABKAhCHKoCGUhPTE1TLlR5cGVz",
+            "LlRlbmFuY3lDb25maWdiBnByb3RvMw=="));
       descriptor = pbr::FileDescriptor.FromGeneratedCode(descriptorData,
           new pbr::FileDescriptor[] { global::HOLMS.Types.TenancyConfig.Indicators.PropertyIndicatorReflection.Descriptor, },
           new pbr::GeneratedClrTypeInfo(null, new pbr::GeneratedClrTypeInfo[] {
-            new pbr::GeneratedClrTypeInfo(typeof(global::HOLMS.Types.TenancyConfig.PropertyEmailSenderConfig), global::HOLMS.Types.TenancyConfig.PropertyEmailSenderConfig.Parser, new[]{ "Property", "PostmarkEmailSenderAddress", "PostmarkApiKey" }, null, null, null)
+            new pbr::GeneratedClrTypeInfo(typeof(global::HOLMS.Types.TenancyConfig.PropertyEmailSenderConfig), global::HOLMS.Types.TenancyConfig.PropertyEmailSenderConfig.Parser, new[]{ "Property", "PostmarkEmailSenderAddress", "PostmarkApiKey", "DisableAutomatedCorrespondence" }, null, null, null)
           }));
     }
     #endregion
@@ -67,6 +68,7 @@ namespace HOLMS.Types.TenancyConfig {
       Property = other.property_ != null ? other.Property.Clone() : null;
       postmarkEmailSenderAddress_ = other.postmarkEmailSenderAddress_;
       postmarkApiKey_ = other.postmarkApiKey_;
+      disableAutomatedCorrespondence_ = other.disableAutomatedCorrespondence_;
     }
 
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
@@ -107,6 +109,17 @@ namespace HOLMS.Types.TenancyConfig {
       }
     }
 
+    /// <summary>Field number for the "disable_automated_correspondence" field.</summary>
+    public const int DisableAutomatedCorrespondenceFieldNumber = 4;
+    private bool disableAutomatedCorrespondence_;
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    public bool DisableAutomatedCorrespondence {
+      get { return disableAutomatedCorrespondence_; }
+      set {
+        disableAutomatedCorrespondence_ = value;
+      }
+    }
+
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
     public override bool Equals(object other) {
       return Equals(other as PropertyEmailSenderConfig);
@@ -123,6 +136,7 @@ namespace HOLMS.Types.TenancyConfig {
       if (!object.Equals(Property, other.Property)) return false;
       if (PostmarkEmailSenderAddress != other.PostmarkEmailSenderAddress) return false;
       if (PostmarkApiKey != other.PostmarkApiKey) return false;
+      if (DisableAutomatedCorrespondence != other.DisableAutomatedCorrespondence) return false;
       return true;
     }
 
@@ -132,6 +146,7 @@ namespace HOLMS.Types.TenancyConfig {
       if (property_ != null) hash ^= Property.GetHashCode();
       if (PostmarkEmailSenderAddress.Length != 0) hash ^= PostmarkEmailSenderAddress.GetHashCode();
       if (PostmarkApiKey.Length != 0) hash ^= PostmarkApiKey.GetHashCode();
+      if (DisableAutomatedCorrespondence != false) hash ^= DisableAutomatedCorrespondence.GetHashCode();
       return hash;
     }
 
@@ -154,6 +169,10 @@ namespace HOLMS.Types.TenancyConfig {
         output.WriteRawTag(26);
         output.WriteString(PostmarkApiKey);
       }
+      if (DisableAutomatedCorrespondence != false) {
+        output.WriteRawTag(32);
+        output.WriteBool(DisableAutomatedCorrespondence);
+      }
     }
 
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
@@ -167,6 +186,9 @@ namespace HOLMS.Types.TenancyConfig {
       }
       if (PostmarkApiKey.Length != 0) {
         size += 1 + pb::CodedOutputStream.ComputeStringSize(PostmarkApiKey);
+      }
+      if (DisableAutomatedCorrespondence != false) {
+        size += 1 + 1;
       }
       return size;
     }
@@ -187,6 +209,9 @@ namespace HOLMS.Types.TenancyConfig {
       }
       if (other.PostmarkApiKey.Length != 0) {
         PostmarkApiKey = other.PostmarkApiKey;
+      }
+      if (other.DisableAutomatedCorrespondence != false) {
+        DisableAutomatedCorrespondence = other.DisableAutomatedCorrespondence;
       }
     }
 
@@ -211,6 +236,10 @@ namespace HOLMS.Types.TenancyConfig {
           }
           case 26: {
             PostmarkApiKey = input.ReadString();
+            break;
+          }
+          case 32: {
+            DisableAutomatedCorrespondence = input.ReadBool();
             break;
           }
         }
