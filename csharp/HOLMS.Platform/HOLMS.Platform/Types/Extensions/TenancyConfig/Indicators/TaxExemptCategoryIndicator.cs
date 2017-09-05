0c6f0bde82afa93cb3e3dab3766258775fc227a8
pcsouza <souza.patrick@gmail.com>
2017-09-05 11:49:41 -0700

    Adds helper extensions

---
+++ csharp/HOLMS.Platform/HOLMS.Platform/Types/Extensions/TenancyConfig/Indicators/TaxExemptCategoryIndicator.cs
@@ -0,0 +1,15 @@
+﻿using HOLMS.Types.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HOLMS.Types.Primitive;
+
+namespace HOLMS.Types.TenancyConfig.Indicators {
+    public partial class TaxExemptCategoryIndicator : EntityIndicator<TaxExemptCategoryIndicator> {
+        public override Uuid GetUuidID() {
+            return Id;
+        }
+    }
+}
