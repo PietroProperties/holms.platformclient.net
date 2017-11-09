eeac2f0fd2f78191ad31e059468b8f7ddd880353
pcsouza <souza.patrick@gmail.com>
2017-11-08 16:24:19 -0800

    [#149938230] Fixes bug with == operator
    EntityDTO had a bug where if the indicator of a dto was null, calling dto
    == null would return true. This was happening because the null object was
    getting cast up as the dto's class, and then the equality comparer would
    compare dto.indicator == null.
    
    This is fixed by a small change in the equality comparer that uses an XOR
    statement to handle references to null before comparing the indicator.
    
    Added tests as well to cover this behavior

---
+++ csharp/HOLMS.Platform/HOLMS.Platform.Tests/Tests/EntityDTOTests.cs
@@ -0,0 +1,40 @@
+﻿using HOLMS.Types.Money.Accounting;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOLMS.Platform.Tests.Tests {
+    class EntityDTOTests {
+        [Test]
+        public void EqualsOperatorComparesEntityNotIndicatorToNull() {
+            var line = new AccountingTransactionLine() { };
+            var actual = line == null;
+            var actualReversed = null == line;
+            Assert.AreEqual(false, actual);
+            Assert.AreEqual(false, actualReversed);
+        }
+
+        [Test]
+        public void MutualVoidEntityComparisonReturnsTrue() {
+            AccountingTransactionLine a = null;
+            AccountingTransactionLine b = null;
+            var actual = a == b;
+            var actualReversed = b == a;
+            Assert.AreEqual(true, actual);
+            Assert.AreEqual(true, actualReversed);
+        }
+
+        [Test]
+        public void VoidEntityVoidIndicatorComparisonReturnsTrue() {
+            AccountingTransactionLine a = null;
+            AccountingTransactionLineIndicator b = null;
+            var actual = a == b;
+            var actualReversed = b == a;
+            Assert.AreEqual(true, actual);
+            Assert.AreEqual(true, actualReversed);
+        }
+    }
+}
