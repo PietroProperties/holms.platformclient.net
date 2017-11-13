b1618a7081a842e51db8349240048e947ec901e1
pcsouza <souza.patrick@gmail.com>
2017-11-13 12:49:38 -0800

    [#152740311] Untaxed/Taxed reservation quoting

---
+++ csharp/HOLMS.Platform/HOLMS.Platform/Types/Extensions/Folio/TaxedReservationQuote.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HOLMS.Types.Folio
+{
+    public partial class TaxedReservationQuote
+    {
+        public Primitive.MonetaryAmount NetPrice {
+            get {
+                var total = LodgingPriceSubtotal.ToDD + IncidentalPriceSubtotal.ToDD
+                    + TaxFeeTotals.Sum(t => t.QuotedTaxFeeAmount.ToDD.Amount);
+                return total.ToPb;
+            }
+        }
+    }
+}
