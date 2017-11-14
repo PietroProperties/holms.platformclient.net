f8e331d8119694e1d5f1661fcb706507cd16eb90
pcsouza <souza.patrick@gmail.com>
2017-11-13 16:07:29 -0800

    Fix for misformatted bracing

--- csharp/HOLMS.Platform/HOLMS.Platform/Types/Extensions/Folio/TaxedReservationQuote.cs
+++ csharp/HOLMS.Platform/HOLMS.Platform/Types/Extensions/Folio/TaxedReservationQuote.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace HOLMS.Types.Folio
-{
-    public partial class TaxedReservationQuote
-    {
+namespace HOLMS.Types.Folio {
+    public partial class TaxedReservationQuote {
         public Primitive.MonetaryAmount NetPrice {
             get {
                 var total = PretaxLodgingPriceSubtotal.ToDD + PretaxIncidentalPriceSubtotal.ToDD
