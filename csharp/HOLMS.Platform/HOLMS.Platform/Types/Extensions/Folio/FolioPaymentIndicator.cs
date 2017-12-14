1f9926c1f63b1641178464b0be56f78fe9cc9af8
David R. Albrecht <albrecht.dr@gmail.com>
2017-12-13 17:45:20 -0800

    Add FolioPaymentIndicator as an indicator (doh)

--- csharp/HOLMS.Platform/HOLMS.Platform/Types/Extensions/Folio/FolioPaymentIndicator.cs
+++ csharp/HOLMS.Platform/HOLMS.Platform/Types/Extensions/Folio/FolioPaymentIndicator.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using HOLMS.Support.Conversions;
+using HOLMS.Types.Extensions;
+using HOLMS.Types.Primitive;
 
 namespace HOLMS.Types.Folio {
-    public partial class FolioPaymentIndicator {
+    public partial class FolioPaymentIndicator : EntityIndicator<FolioPaymentIndicator> {
         public FolioPaymentIndicator(Guid id) {
             Id = id.ToUUID();
         }
+
+        public override Uuid GetUuidID() => Id;
     }
 }
