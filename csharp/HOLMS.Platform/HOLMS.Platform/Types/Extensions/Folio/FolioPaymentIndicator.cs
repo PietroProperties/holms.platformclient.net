d16b83ea40bf879d0351afa58217031b6552e788
David R. Albrecht <albrecht.dr@gmail.com>
2017-12-13 17:32:57 -0800

    Add Guid-based constructor to FolioPaymentIndicator

---
+++ csharp/HOLMS.Platform/HOLMS.Platform/Types/Extensions/Folio/FolioPaymentIndicator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using HOLMS.Support.Conversions;
+
+namespace HOLMS.Types.Folio {
+    public partial class FolioPaymentIndicator {
+        public FolioPaymentIndicator(Guid id) {
+            Id = id.ToUUID();
+        }
+    }
+}
