bf137e2668ad9a1c37baa2b8d5ce3337b4c35183
pcsouza <souza.patrick@gmail.com>
2018-02-28 15:17:41 -0800

    [#154650718] folio close notification/topic

---
+++ csharp/HOLMS.Platform/HOLMS.Platform/Types/Topics/FolioContextTopics.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HOLMS.Platform.Types.Topics {
+    public class FolioContextTopics {
+        public static string ReservationsFolioClose = "folio.reservation_close";
+    }
+}
