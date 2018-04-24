95b22933fbc670df741d5680b2c9768965f32120
pcsouza <souza.patrick@gmail.com>
2018-04-24 10:46:22 -0700

    [#156387172] Rename Policy/Rule Violation
    This is a stand-in for only guest count violations, so it's being renamed
    to reflect that

--- csharp/HOLMS.Platform/HOLMS.Platform/Types/out/CheckInRequestResult.cs
+++ csharp/HOLMS.Platform/HOLMS.Platform/Types/out/CheckInRequestResult.cs
@@ -23,14 +23,15 @@ namespace HOLMS.Types.Booking.CheckIn {
       byte[] descriptorData = global::System.Convert.FromBase64String(
           string.Concat(
             "Ci5ib29raW5nL2NoZWNrX2luL2NoZWNrX2luX3JlcXVlc3RfcmVzdWx0LnBy",
-            "b3RvEhxob2xtcy50eXBlcy5ib29raW5nLmNoZWNrX2luKokCChRDaGVja0lu",
+            "b3RvEhxob2xtcy50eXBlcy5ib29raW5nLmNoZWNrX2luKpcCChRDaGVja0lu",
             "UmVxdWVzdFJlc3VsdBIGCgJPSxAAEhYKEkFMUkVBRFlfQ0hFQ0tFRF9JThAB",
             "EhkKFU1JU1NJTkdfQVNTSUdORURfUk9PTRACEhAKDE1JU1NJTkdfQ0FSRBAD",
             "EhYKEk1JU1NJTkdfR1VFU1RfTkFNRRAEEhcKE05PX09QRU5fUkVTRVJWQVRJ",
-            "T04QBRIeChpSRVNFUlZBVElPTl9SVUxFX1ZJT0xBVElPThAGEhgKFFRPT19F",
-            "QVJMWV9UT19DSEVDS0lOEAcSHgoaUkVTRVJWQVRJT05fTk9UX0dVQVJBTlRF",
-            "RUQQCBIZChVST09NX0FMUkVBRFlfT0NDVVBJRUQQCUIvWg9ib29raW5nL2No",
-            "ZWNraW6qAhtIT0xNUy5UeXBlcy5Cb29raW5nLkNoZWNrSW5iBnByb3RvMw=="));
+            "T04QBRIsCihQT0xJQ1lfVklPTEFUSU9OX1RPT19NQU5ZX0dVRVNUU19JTl9S",
+            "T09NEAYSGAoUVE9PX0VBUkxZX1RPX0NIRUNLSU4QBxIeChpSRVNFUlZBVElP",
+            "Tl9OT1RfR1VBUkFOVEVFRBAIEhkKFVJPT01fQUxSRUFEWV9PQ0NVUElFRBAJ",
+            "Qi9aD2Jvb2tpbmcvY2hlY2tpbqoCG0hPTE1TLlR5cGVzLkJvb2tpbmcuQ2hl",
+            "Y2tJbmIGcHJvdG8z"));
       descriptor = pbr::FileDescriptor.FromGeneratedCode(descriptorData,
           new pbr::FileDescriptor[] { },
           new pbr::GeneratedClrTypeInfo(new[] {typeof(global::HOLMS.Types.Booking.CheckIn.CheckInRequestResult), }, null));
@@ -46,7 +47,7 @@ namespace HOLMS.Types.Booking.CheckIn {
     [pbr::OriginalName("MISSING_CARD")] MissingCard = 3,
     [pbr::OriginalName("MISSING_GUEST_NAME")] MissingGuestName = 4,
     [pbr::OriginalName("NO_OPEN_RESERVATION")] NoOpenReservation = 5,
-    [pbr::OriginalName("RESERVATION_RULE_VIOLATION")] ReservationRuleViolation = 6,
+    [pbr::OriginalName("POLICY_VIOLATION_TOO_MANY_GUESTS_IN_ROOM")] PolicyViolationTooManyGuestsInRoom = 6,
     [pbr::OriginalName("TOO_EARLY_TO_CHECKIN")] TooEarlyToCheckin = 7,
     [pbr::OriginalName("RESERVATION_NOT_GUARANTEED")] ReservationNotGuaranteed = 8,
     [pbr::OriginalName("ROOM_ALREADY_OCCUPIED")] RoomAlreadyOccupied = 9,
