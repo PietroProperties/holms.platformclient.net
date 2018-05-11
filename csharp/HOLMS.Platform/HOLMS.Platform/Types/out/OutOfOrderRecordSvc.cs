5615e109b4ccf8941e77552a72c4d6e474851152
David R. Albrecht <albrecht.dr@gmail.com>
2018-05-11 14:21:47 -0700

    Add occupancy conflict results for OOO CU (CrUd)
    [#157525007]

--- csharp/HOLMS.Platform/HOLMS.Platform/Types/out/OutOfOrderRecordSvc.cs
+++ csharp/HOLMS.Platform/HOLMS.Platform/Types/out/OutOfOrderRecordSvc.cs
@@ -42,56 +42,52 @@ namespace HOLMS.Types.Operations.RPC {
             "LnR5cGVzLm9wZXJhdGlvbnMucnBjLk91dE9mT3JkZXJSZWNvcmRVcGRhdGVS",
             "ZXN1bHQSRQoGcmVjb3JkGAIgASgLMjUuaG9sbXMudHlwZXMub3BlcmF0aW9u",
             "cy5vdXRfb2Zfb3JkZXIuT3V0T2ZPcmRlclJlY29yZCJqCh5PdXRPZk9yZGVy",
-            "UmVjb3JkQ2FuY2VsUmVzcG9uc2USSAoGcmVzdWx0GAEgASgOMjguaG9sbXMu",
-            "dHlwZXMub3BlcmF0aW9ucy5ycGMuT3V0T2ZPcmRlclJlY29yZENhbmNlbFJl",
-            "c3VsdCJqCh5PdXRPZk9yZGVyUmVjb3JkRGVsZXRlUmVzcG9uc2USSAoGcmVz",
-            "dWx0GAEgASgOMjguaG9sbXMudHlwZXMub3BlcmF0aW9ucy5ycGMuT3V0T2ZP",
-            "cmRlclJlY29yZERlbGV0ZVJlc3VsdCphCh1PdXRPZk9yZGVyUmVjb3JkR2V0",
-            "QnlJZFJlc3VsdBIdChlPVVRfT0ZfT1JERVJfUkVDT1JEX0ZPVU5EEAASIQod",
-            "T1VUX09GX09SREVSX1JFQ09SRF9OT1RfRk9VTkQQASqDAgocT3V0T2ZPcmRl",
-            "clJlY29yZENyZWF0ZVJlc3VsdBImCiJPVVRfT0ZfT1JERVJfUkVDT1JEX0NS",
-            "RUFURV9TVUNDRVNTEAASLQopT1VUX09GX09SREVSX1JFQ09SRF9DUkVBVEVf",
-            "TUFJTlRfQ09ORkxJQ1QQARIzCi9PVVRfT0ZfT1JERVJfUkVDT1JEX0NSRUFU",
-            "RV9SRVNFUlZBVElPTl9DT05GTElDVBACEi0KKU9VVF9PRl9PUkRFUl9SRUNP",
-            "UkRfQ1JFQVRFX1JPT01fTk9UX0ZPVU5EEAMSKAokT1VUX09GX09SREVSX1JF",
-            "Q09SRF9DUkVBVEVfTk9fU1VQUExZEAQqvAIKHE91dE9mT3JkZXJSZWNvcmRV",
-            "cGRhdGVSZXN1bHQSKQolT1VUX09GX09SREVSX1JFQ09SRF9VUERBVEVfU1VD",
-            "Q0VTU0ZVTBAAEi4KKk9VVF9PRl9PUkRFUl9SRUNPUkRfVVBEQVRFX1BSSU9S",
-            "X05PVF9GT1VORBABEi0KKU9VVF9PRl9PUkRFUl9SRUNPUkRfVVBEQVRFX01B",
-            "SU5UX0NPTkZMSUNUEAISMwovT1VUX09GX09SREVSX1JFQ09SRF9VUERBVEVf",
-            "UkVTRVJWQVRJT05fQ09ORkxJQ1QQAxItCilPVVRfT0ZfT1JERVJfUkVDT1JE",
-            "X1VQREFURV9ST09NX05PVF9GT1VORBAEEi4KKk9VVF9PRl9PUkRFUl9SRUNP",
-            "UkRfVVBEQVRFX05PX0FWQUlMQUJJTElUWRAFKnkKHE91dE9mT3JkZXJSZWNv",
-            "cmRDYW5jZWxSZXN1bHQSKQolT1VUX09GX09SREVSX1JFQ09SRF9DQU5DRUxf",
-            "U1VDQ0VTU0ZVTBAAEi4KKk9VVF9PRl9PUkRFUl9SRUNPUkRfQ0FOQ0VMX1BS",
-            "SU9SX05PVF9GT1VORBABKnkKHE91dE9mT3JkZXJSZWNvcmREZWxldGVSZXN1",
-            "bHQSKQolT1VUX09GX09SREVSX1JFQ09SRF9ERUxFVEVfU1VDQ0VTU0ZVTBAA",
-            "Ei4KKk9VVF9PRl9PUkRFUl9SRUNPUkRfREVMRVRFX1BSSU9SX05PVF9GT1VO",
-            "RBABMpwFChNPdXRPZk9yZGVyUmVjb3JkU3ZjEnsKA0FsbBI4LmhvbG1zLnR5",
-            "cGVzLnRlbmFuY3lfY29uZmlnLmluZGljYXRvcnMuUHJvcGVydHlJbmRpY2F0",
-            "b3IaOi5ob2xtcy50eXBlcy5vcGVyYXRpb25zLnJwYy5PdXRPZk9yZGVyUmVj",
-            "b3JkU3ZjQWxsUmVzcG9uc2UShgEKB0dldEJ5SWQSPi5ob2xtcy50eXBlcy5v",
-            "cGVyYXRpb25zLm91dF9vZl9vcmRlci5PdXRPZk9yZGVyUmVjb3JkSW5kaWNh",
-            "dG9yGjsuaG9sbXMudHlwZXMub3BlcmF0aW9ucy5ycGMuT3V0T2ZPcmRlclJl",
-            "Y29yZEdldEJ5SWRSZXNwb25zZRJ7CgZDcmVhdGUSNS5ob2xtcy50eXBlcy5v",
-            "cGVyYXRpb25zLm91dF9vZl9vcmRlci5PdXRPZk9yZGVyUmVjb3JkGjouaG9s",
-            "bXMudHlwZXMub3BlcmF0aW9ucy5ycGMuT3V0T2ZPcmRlclJlY29yZENyZWF0",
-            "ZVJlc3BvbnNlEnsKBlVwZGF0ZRI1LmhvbG1zLnR5cGVzLm9wZXJhdGlvbnMu",
-            "b3V0X29mX29yZGVyLk91dE9mT3JkZXJSZWNvcmQaOi5ob2xtcy50eXBlcy5v",
-            "cGVyYXRpb25zLnJwYy5PdXRPZk9yZGVyUmVjb3JkVXBkYXRlUmVzcG9uc2US",
-            "hAEKBkRlbGV0ZRI+LmhvbG1zLnR5cGVzLm9wZXJhdGlvbnMub3V0X29mX29y",
-            "ZGVyLk91dE9mT3JkZXJSZWNvcmRJbmRpY2F0b3IaOi5ob2xtcy50eXBlcy5v",
-            "cGVyYXRpb25zLnJwYy5PdXRPZk9yZGVyUmVjb3JkRGVsZXRlUmVzcG9uc2VC",
-            "LVoOb3BlcmF0aW9ucy9ycGOqAhpIT0xNUy5UeXBlcy5PcGVyYXRpb25zLlJQ",
-            "Q2IGcHJvdG8z"));
+            "UmVjb3JkRGVsZXRlUmVzcG9uc2USSAoGcmVzdWx0GAEgASgOMjguaG9sbXMu",
+            "dHlwZXMub3BlcmF0aW9ucy5ycGMuT3V0T2ZPcmRlclJlY29yZERlbGV0ZVJl",
+            "c3VsdCphCh1PdXRPZk9yZGVyUmVjb3JkR2V0QnlJZFJlc3VsdBIdChlPVVRf",
+            "T0ZfT1JERVJfUkVDT1JEX0ZPVU5EEAASIQodT1VUX09GX09SREVSX1JFQ09S",
+            "RF9OT1RfRk9VTkQQASq2AgocT3V0T2ZPcmRlclJlY29yZENyZWF0ZVJlc3Vs",
+            "dBImCiJPVVRfT0ZfT1JERVJfUkVDT1JEX0NSRUFURV9TVUNDRVNTEAASLQop",
+            "T1VUX09GX09SREVSX1JFQ09SRF9DUkVBVEVfTUFJTlRfQ09ORkxJQ1QQARIz",
+            "Ci9PVVRfT0ZfT1JERVJfUkVDT1JEX0NSRUFURV9SRVNFUlZBVElPTl9DT05G",
+            "TElDVBACEi0KKU9VVF9PRl9PUkRFUl9SRUNPUkRfQ1JFQVRFX1JPT01fTk9U",
+            "X0ZPVU5EEAMSKAokT1VUX09GX09SREVSX1JFQ09SRF9DUkVBVEVfTk9fU1VQ",
+            "UExZEAQSMQotT1VUX09GX09SREVSX1JFQ09SRF9DUkVBVEVfT0NDVVBBTkNZ",
+            "X0NPTkZMSUNUEAUq7wIKHE91dE9mT3JkZXJSZWNvcmRVcGRhdGVSZXN1bHQS",
+            "KQolT1VUX09GX09SREVSX1JFQ09SRF9VUERBVEVfU1VDQ0VTU0ZVTBAAEi4K",
+            "Kk9VVF9PRl9PUkRFUl9SRUNPUkRfVVBEQVRFX1BSSU9SX05PVF9GT1VORBAB",
+            "Ei0KKU9VVF9PRl9PUkRFUl9SRUNPUkRfVVBEQVRFX01BSU5UX0NPTkZMSUNU",
+            "EAISMwovT1VUX09GX09SREVSX1JFQ09SRF9VUERBVEVfUkVTRVJWQVRJT05f",
+            "Q09ORkxJQ1QQAxItCilPVVRfT0ZfT1JERVJfUkVDT1JEX1VQREFURV9ST09N",
+            "X05PVF9GT1VORBAEEi4KKk9VVF9PRl9PUkRFUl9SRUNPUkRfVVBEQVRFX05P",
+            "X0FWQUlMQUJJTElUWRAFEjEKLU9VVF9PRl9PUkRFUl9SRUNPUkRfVVBEQVRF",
+            "X09DQ1VQQU5DWV9DT05GTElDVBAGKnkKHE91dE9mT3JkZXJSZWNvcmREZWxl",
+            "dGVSZXN1bHQSKQolT1VUX09GX09SREVSX1JFQ09SRF9ERUxFVEVfU1VDQ0VT",
+            "U0ZVTBAAEi4KKk9VVF9PRl9PUkRFUl9SRUNPUkRfREVMRVRFX1BSSU9SX05P",
+            "VF9GT1VORBABMpwFChNPdXRPZk9yZGVyUmVjb3JkU3ZjEnsKA0FsbBI4Lmhv",
+            "bG1zLnR5cGVzLnRlbmFuY3lfY29uZmlnLmluZGljYXRvcnMuUHJvcGVydHlJ",
+            "bmRpY2F0b3IaOi5ob2xtcy50eXBlcy5vcGVyYXRpb25zLnJwYy5PdXRPZk9y",
+            "ZGVyUmVjb3JkU3ZjQWxsUmVzcG9uc2UShgEKB0dldEJ5SWQSPi5ob2xtcy50",
+            "eXBlcy5vcGVyYXRpb25zLm91dF9vZl9vcmRlci5PdXRPZk9yZGVyUmVjb3Jk",
+            "SW5kaWNhdG9yGjsuaG9sbXMudHlwZXMub3BlcmF0aW9ucy5ycGMuT3V0T2ZP",
+            "cmRlclJlY29yZEdldEJ5SWRSZXNwb25zZRJ7CgZDcmVhdGUSNS5ob2xtcy50",
+            "eXBlcy5vcGVyYXRpb25zLm91dF9vZl9vcmRlci5PdXRPZk9yZGVyUmVjb3Jk",
+            "GjouaG9sbXMudHlwZXMub3BlcmF0aW9ucy5ycGMuT3V0T2ZPcmRlclJlY29y",
+            "ZENyZWF0ZVJlc3BvbnNlEnsKBlVwZGF0ZRI1LmhvbG1zLnR5cGVzLm9wZXJh",
+            "dGlvbnMub3V0X29mX29yZGVyLk91dE9mT3JkZXJSZWNvcmQaOi5ob2xtcy50",
+            "eXBlcy5vcGVyYXRpb25zLnJwYy5PdXRPZk9yZGVyUmVjb3JkVXBkYXRlUmVz",
+            "cG9uc2UShAEKBkRlbGV0ZRI+LmhvbG1zLnR5cGVzLm9wZXJhdGlvbnMub3V0",
+            "X29mX29yZGVyLk91dE9mT3JkZXJSZWNvcmRJbmRpY2F0b3IaOi5ob2xtcy50",
+            "eXBlcy5vcGVyYXRpb25zLnJwYy5PdXRPZk9yZGVyUmVjb3JkRGVsZXRlUmVz",
+            "cG9uc2VCLVoOb3BlcmF0aW9ucy9ycGOqAhpIT0xNUy5UeXBlcy5PcGVyYXRp",
+            "b25zLlJQQ2IGcHJvdG8z"));
       descriptor = pbr::FileDescriptor.FromGeneratedCode(descriptorData,
           new pbr::FileDescriptor[] { global::HOLMS.Types.Operations.OutOfOrder.OutOfOrderRecordReflection.Descriptor, global::HOLMS.Types.Operations.OutOfOrder.OutOfOrderRecordIndicatorReflection.Descriptor, global::HOLMS.Types.TenancyConfig.Indicators.PropertyIndicatorReflection.Descriptor, },
-          new pbr::GeneratedClrTypeInfo(new[] {typeof(global::HOLMS.Types.Operations.RPC.OutOfOrderRecordGetByIdResult), typeof(global::HOLMS.Types.Operations.RPC.OutOfOrderRecordCreateResult), typeof(global::HOLMS.Types.Operations.RPC.OutOfOrderRecordUpdateResult), typeof(global::HOLMS.Types.Operations.RPC.OutOfOrderRecordCancelResult), typeof(global::HOLMS.Types.Operations.RPC.OutOfOrderRecordDeleteResult), }, new pbr::GeneratedClrTypeInfo[] {
+          new pbr::GeneratedClrTypeInfo(new[] {typeof(global::HOLMS.Types.Operations.RPC.OutOfOrderRecordGetByIdResult), typeof(global::HOLMS.Types.Operations.RPC.OutOfOrderRecordCreateResult), typeof(global::HOLMS.Types.Operations.RPC.OutOfOrderRecordUpdateResult), typeof(global::HOLMS.Types.Operations.RPC.OutOfOrderRecordDeleteResult), }, new pbr::GeneratedClrTypeInfo[] {
             new pbr::GeneratedClrTypeInfo(typeof(global::HOLMS.Types.Operations.RPC.OutOfOrderRecordSvcAllResponse), global::HOLMS.Types.Operations.RPC.OutOfOrderRecordSvcAllResponse.Parser, new[]{ "Records" }, null, null, null),
             new pbr::GeneratedClrTypeInfo(typeof(global::HOLMS.Types.Operations.RPC.OutOfOrderRecordGetByIdResponse), global::HOLMS.Types.Operations.RPC.OutOfOrderRecordGetByIdResponse.Parser, new[]{ "Result", "Record" }, null, null, null),
             new pbr::GeneratedClrTypeInfo(typeof(global::HOLMS.Types.Operations.RPC.OutOfOrderRecordCreateResponse), global::HOLMS.Types.Operations.RPC.OutOfOrderRecordCreateResponse.Parser, new[]{ "Result", "Record" }, null, null, null),
             new pbr::GeneratedClrTypeInfo(typeof(global::HOLMS.Types.Operations.RPC.OutOfOrderRecordUpdateResponse), global::HOLMS.Types.Operations.RPC.OutOfOrderRecordUpdateResponse.Parser, new[]{ "Result", "Record" }, null, null, null),
-            new pbr::GeneratedClrTypeInfo(typeof(global::HOLMS.Types.Operations.RPC.OutOfOrderRecordCancelResponse), global::HOLMS.Types.Operations.RPC.OutOfOrderRecordCancelResponse.Parser, new[]{ "Result" }, null, null, null),
             new pbr::GeneratedClrTypeInfo(typeof(global::HOLMS.Types.Operations.RPC.OutOfOrderRecordDeleteResponse), global::HOLMS.Types.Operations.RPC.OutOfOrderRecordDeleteResponse.Parser, new[]{ "Result" }, null, null, null)
           }));
     }
@@ -110,6 +106,7 @@ namespace HOLMS.Types.Operations.RPC {
     [pbr::OriginalName("OUT_OF_ORDER_RECORD_CREATE_RESERVATION_CONFLICT")] OutOfOrderRecordCreateReservationConflict = 2,
     [pbr::OriginalName("OUT_OF_ORDER_RECORD_CREATE_ROOM_NOT_FOUND")] OutOfOrderRecordCreateRoomNotFound = 3,
     [pbr::OriginalName("OUT_OF_ORDER_RECORD_CREATE_NO_SUPPLY")] OutOfOrderRecordCreateNoSupply = 4,
+    [pbr::OriginalName("OUT_OF_ORDER_RECORD_CREATE_OCCUPANCY_CONFLICT")] OutOfOrderRecordCreateOccupancyConflict = 5,
   }
 
   public enum OutOfOrderRecordUpdateResult {
@@ -119,11 +116,7 @@ namespace HOLMS.Types.Operations.RPC {
     [pbr::OriginalName("OUT_OF_ORDER_RECORD_UPDATE_RESERVATION_CONFLICT")] OutOfOrderRecordUpdateReservationConflict = 3,
     [pbr::OriginalName("OUT_OF_ORDER_RECORD_UPDATE_ROOM_NOT_FOUND")] OutOfOrderRecordUpdateRoomNotFound = 4,
     [pbr::OriginalName("OUT_OF_ORDER_RECORD_UPDATE_NO_AVAILABILITY")] OutOfOrderRecordUpdateNoAvailability = 5,
-  }
-
-  public enum OutOfOrderRecordCancelResult {
-    [pbr::OriginalName("OUT_OF_ORDER_RECORD_CANCEL_SUCCESSFUL")] OutOfOrderRecordCancelSuccessful = 0,
-    [pbr::OriginalName("OUT_OF_ORDER_RECORD_CANCEL_PRIOR_NOT_FOUND")] OutOfOrderRecordCancelPriorNotFound = 1,
+    [pbr::OriginalName("OUT_OF_ORDER_RECORD_UPDATE_OCCUPANCY_CONFLICT")] OutOfOrderRecordUpdateOccupancyConflict = 6,
   }
 
   public enum OutOfOrderRecordDeleteResult {
@@ -696,123 +689,6 @@ namespace HOLMS.Types.Operations.RPC {
 
   }
 
-  public sealed partial class OutOfOrderRecordCancelResponse : pb::IMessage<OutOfOrderRecordCancelResponse> {
-    private static readonly pb::MessageParser<OutOfOrderRecordCancelResponse> _parser = new pb::MessageParser<OutOfOrderRecordCancelResponse>(() => new OutOfOrderRecordCancelResponse());
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-    public static pb::MessageParser<OutOfOrderRecordCancelResponse> Parser { get { return _parser; } }
-
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-    public static pbr::MessageDescriptor Descriptor {
-      get { return global::HOLMS.Types.Operations.RPC.OutOfOrderRecordSvcReflection.Descriptor.MessageTypes[4]; }
-    }
-
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-    pbr::MessageDescriptor pb::IMessage.Descriptor {
-      get { return Descriptor; }
-    }
-
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-    public OutOfOrderRecordCancelResponse() {
-      OnConstruction();
-    }
-
-    partial void OnConstruction();
-
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-    public OutOfOrderRecordCancelResponse(OutOfOrderRecordCancelResponse other) : this() {
-      result_ = other.result_;
-    }
-
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-    public OutOfOrderRecordCancelResponse Clone() {
-      return new OutOfOrderRecordCancelResponse(this);
-    }
-
-    /// <summary>Field number for the "result" field.</summary>
-    public const int ResultFieldNumber = 1;
-    private global::HOLMS.Types.Operations.RPC.OutOfOrderRecordCancelResult result_ = 0;
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-    public global::HOLMS.Types.Operations.RPC.OutOfOrderRecordCancelResult Result {
-      get { return result_; }
-      set {
-        result_ = value;
-      }
-    }
-
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-    public override bool Equals(object other) {
-      return Equals(other as OutOfOrderRecordCancelResponse);
-    }
-
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-    public bool Equals(OutOfOrderRecordCancelResponse other) {
-      if (ReferenceEquals(other, null)) {
-        return false;
-      }
-      if (ReferenceEquals(other, this)) {
-        return true;
-      }
-      if (Result != other.Result) return false;
-      return true;
-    }
-
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-    public override int GetHashCode() {
-      int hash = 1;
-      if (Result != 0) hash ^= Result.GetHashCode();
-      return hash;
-    }
-
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-    public override string ToString() {
-      return pb::JsonFormatter.ToDiagnosticString(this);
-    }
-
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-    public void WriteTo(pb::CodedOutputStream output) {
-      if (Result != 0) {
-        output.WriteRawTag(8);
-        output.WriteEnum((int) Result);
-      }
-    }
-
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-    public int CalculateSize() {
-      int size = 0;
-      if (Result != 0) {
-        size += 1 + pb::CodedOutputStream.ComputeEnumSize((int) Result);
-      }
-      return size;
-    }
-
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-    public void MergeFrom(OutOfOrderRecordCancelResponse other) {
-      if (other == null) {
-        return;
-      }
-      if (other.Result != 0) {
-        Result = other.Result;
-      }
-    }
-
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-    public void MergeFrom(pb::CodedInputStream input) {
-      uint tag;
-      while ((tag = input.ReadTag()) != 0) {
-        switch(tag) {
-          default:
-            input.SkipLastField();
-            break;
-          case 8: {
-            result_ = (global::HOLMS.Types.Operations.RPC.OutOfOrderRecordCancelResult) input.ReadEnum();
-            break;
-          }
-        }
-      }
-    }
-
-  }
-
   public sealed partial class OutOfOrderRecordDeleteResponse : pb::IMessage<OutOfOrderRecordDeleteResponse> {
     private static readonly pb::MessageParser<OutOfOrderRecordDeleteResponse> _parser = new pb::MessageParser<OutOfOrderRecordDeleteResponse>(() => new OutOfOrderRecordDeleteResponse());
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
@@ -820,7 +696,7 @@ namespace HOLMS.Types.Operations.RPC {
 
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
     public static pbr::MessageDescriptor Descriptor {
-      get { return global::HOLMS.Types.Operations.RPC.OutOfOrderRecordSvcReflection.Descriptor.MessageTypes[5]; }
+      get { return global::HOLMS.Types.Operations.RPC.OutOfOrderRecordSvcReflection.Descriptor.MessageTypes[4]; }
     }
 
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
