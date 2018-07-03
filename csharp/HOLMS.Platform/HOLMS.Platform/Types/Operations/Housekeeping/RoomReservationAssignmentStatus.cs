b4eb8a94c6939575e36b99cb94a0a41821f73bff
David R. Albrecht <albrecht.dr@gmail.com>
2018-07-03 16:53:12 -0700

    Add ToEnglish() extension method to RoomReservationAssignmentStatus
    Allows conversion of names like "VacantVacant" into more English-sounding
    "Vacant".

---
+++ csharp/HOLMS.Platform/HOLMS.Platform/Types/Operations/Housekeeping/RoomReservationAssignmentStatus.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HOLMS.Types.Operations.Housekeeping {
+    public static class RoomReservationAssignmentStatusExtensions {
+        public static string ToEnglish(this RoomReservationAssignmentStatus rras) {
+            switch (rras) {
+                case RoomReservationAssignmentStatus.VacantVacant:
+                    return "Vacant";
+                case RoomReservationAssignmentStatus.Stayover:
+                    return "Stayover";
+                case RoomReservationAssignmentStatus.LeavingNoneIncoming:
+                    return "Departure";
+                case RoomReservationAssignmentStatus.LeftNoneIncoming:
+                    return "Checked Out";
+                case RoomReservationAssignmentStatus.LeavingWithIncoming:
+                    return "Departure/Arrival";
+                case RoomReservationAssignmentStatus.LeftWithIncoming:
+                    return "Checked Out/Arrival";
+                case RoomReservationAssignmentStatus.VacantWithIncoming:
+                    return "Arrival";
+                case RoomReservationAssignmentStatus.ArrivedAfterVacancy:
+                    return "Checked In";
+                case RoomReservationAssignmentStatus.RoomReservationAssignmentOutOfOrder:
+                    return "Out of order";
+                case RoomReservationAssignmentStatus.Other:
+                    return "Other";
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
